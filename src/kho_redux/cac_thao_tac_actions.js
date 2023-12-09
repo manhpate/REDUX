fbbf6163dea8a8586b44d91653006e0088d816f5
manhpate <manhpate@gmail.com>
2023-12-09 13:10:05 +0700

    todo

--- src/kho_redux/cac_thao_tac_actions.js
+++ src/kho_redux/cac_thao_tac_actions.js
@@ -1,33 +1,33 @@
 
-export const addThuMuc = (data) => {
-    return {
-        type: 'themCacMuc/them',
-        payload: data,
-    }
-}
-export const addThuMuc_selectCheckBox = (addThuMucId) => {
-    return {
-        type: 'themCacMuc/selectCheckBox',
-        payload: addThuMucId,
-    }
-}
-export const searchFilterChange = (textSearch) => {
-    return {
-        type: 'cacThaoTacTimKiem/thaydoitimkiem',
-        payload: textSearch,
-    }
-}
+// export const addThuMuc = (data) => {
+//     return {
+//         type: 'themCacMuc/them',
+//         payload: data,
+//     }
+// }
+// export const addThuMuc_selectCheckBox = (addThuMucId) => {
+//     return {
+//         type: 'themCacMuc/selectCheckBox',
+//         payload: addThuMucId,
+//     }
+// }
+// export const searchFilterChange = (textSearch) => {
+//     return {
+//         type: 'cacThaoTacTimKiem/thaydoitimkiem',
+//         payload: textSearch,
+//     }
+// }
 
-export const searchTheo_mucChon_HoanThanh_Change = (mucLuaChon) => {
-    return {
-        type: 'cacThaoTacTimKiem/searchTheo_mucChon_HoanThanh_Change',
-        payload: mucLuaChon,
-    }
-}
+// export const searchTheo_mucChon_HoanThanh_Change = (mucLuaChon) => {
+//     return {
+//         type: 'cacThaoTacTimKiem/searchTheo_mucChon_HoanThanh_Change',
+//         payload: mucLuaChon,
+//     }
+// }
 
-export const chon_GiaTris_MucDo_Change = (GiaTris_MucDo) => {
-    return {
-        type: 'cacThaoTacTimKiem/kieuGiaTri',
-        payload: GiaTris_MucDo,
-    }
-}
+// export const chon_GiaTris_MucDo_Change = (GiaTris_MucDo) => {
+//     return {
+//         type: 'cacThaoTacTimKiem/kieuGiaTri',
+//         payload: GiaTris_MucDo,
+//     }
+// }
